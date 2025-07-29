1bfda513f06d8b071253752dcdf296c3546252ea
KendallCHG31 <Kendallchaves8@gmail.com>
2025-07-29 17:21:23 -0600

    Uso de EmailJs

--- public/views/contacto.js
+++ public/views/contacto.js
@@ -8,16 +8,16 @@ document.getElementById('formContacto').addEventListener('submit', function(even
 
   // Envía el formulario usando EmailJS
   emailjs.sendForm(
-    'service_oxxsvyc', // Service ID exacto de tu cuenta EmailJS
-    'template_iprlzex', // Template ID exacto de tu plantilla EmailJS
+    'service_oxxsvyc', // Service ID exacto  cuenta EmailJS
+    'template_iprlzex', // Template ID 
     this // 'this' es el formulario HTML
   )
   .then(function() {
-    // Si se envía correctamente
+    
     alert('Mensaje enviado correctamente. Gracias por contactarnos.');
     document.getElementById('formContacto').reset(); // Limpia formulario
   }, function(error) {
-    // Si falla, muestra error en consola y alerta al usuario
+    
     console.log('FAILED...', error);
     alert('Error al enviar mensaje. Intente más tarde.');
   });
